10e6064d0ad9c69c7c49c4a60fd6cc997790afb8
Gregory Katsaros <gregorykatsaros@u0094945-tpl-a.ten.thomsonreuters.com>
2018-03-02 12:00:13 +0000

    read from file

--- dummy-ipam-user/ipam-user-server.py
+++ dummy-ipam-user/ipam-user-server.py
@@ -16,7 +16,6 @@ class S(BaseHTTPRequestHandler):
         self._set_headers()
 
         filename = "/opt/ipam_ip"
-        # Open the file as f.
         # The function readlines() reads the file.             
         with open(filename) as f:
         content = f.readlines()
